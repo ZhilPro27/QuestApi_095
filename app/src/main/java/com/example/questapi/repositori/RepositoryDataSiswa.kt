ff55f58cf03df09e616edf9bc5d3576270dbacb8
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-18 12:08:33 +0700

    menambahkan class JaringanRepositoryDataSiswa pada file RepositoryDataSiswa.kt

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -1,8 +1,17 @@
 package com.example.questapi.repositori
 
+import com.example.questapi.apiservices.ServiceApiSiswa
 import com.example.questapi.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa {
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
