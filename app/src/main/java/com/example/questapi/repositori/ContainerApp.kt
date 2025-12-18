ce2277a20626e4f1ab8b4f99ec2a0669b3507a4d
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-18 12:23:38 +0700

    menambahkan class AplikasiDataSiswa pada file ContainerApp.kt

--- app/src/main/java/com/example/questapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi/repositori/ContainerApp.kt
@@ -1,6 +1,6 @@
 package com.example.questapi.repositori
 
-import androidx.compose.ui.geometry.Rect
+import android.app.Application
 import com.example.questapi.apiservices.ServiceApiSiswa
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -42,5 +42,13 @@ class DefaultContainerApp : ContainerApp {
 
     override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
         JaringanRepositoryDataSiswa(retrofitService)
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container : ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
     }
 }
