1b8ed917a07575440c3e497471f6c98c965ecdb1
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-18 12:06:18 +0700

    menambahkan interface RepositoryDataSiswa pada file RepositoryDataSiswa.kt

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,8 @@
 package com.example.questapi.repositori
 
+import com.example.questapi.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
