0cbba8eaac573ffad4bc6778d2ceb780ec874486
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 14:01:29 +0700

    Menambahkan 1 override fungsi getSatuSiwa pada file RepositoryDataSiswa.kt

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -7,7 +7,7 @@ import retrofit2.Response
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
-    suspend fun getSatuSiswa(id: Int, dataSiswa: DataSiswa): DataSiswa
+    suspend fun getSatuSiswa(id: Int): DataSiswa
     suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa) :retrofit2.Response<Void>
     suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
 }
@@ -17,4 +17,5 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int) : DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
