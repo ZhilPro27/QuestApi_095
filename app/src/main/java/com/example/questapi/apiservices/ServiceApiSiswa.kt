204a7a1231fed98129e58b48cb52ddd930ed7d61
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 13:44:51 +0700

    Menambahkan 1 method GET untuk endpoint baca1Teman.php/:id pada file ServiceApiSiswa.kt

--- app/src/main/java/com/example/questapi/apiservices/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi/apiservices/ServiceApiSiswa.kt
@@ -4,6 +4,7 @@ import com.example.questapi.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET("bacateman.php")
@@ -11,4 +12,8 @@ interface ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getStatusSiswa(@Query("id") id: Int): DataSiswa
+
 }
