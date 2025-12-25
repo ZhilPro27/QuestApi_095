f999f22dd600d46a581c4ef207b58464d165db64
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 14:41:30 +0700

    membuat sealed interface pada file DetailViewModel.kt

--- app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.questapi.viewmodel
 
+import com.example.questapi.modeldata.DetailSiswa
+
+sealed interface StatusUiDetail{
+    data class Succes(val satusiswa: DetailSiswa): StatusUiDetail
+    object Error : StatusUiDetail
+    object Loading : StatusUiDetail
+}
