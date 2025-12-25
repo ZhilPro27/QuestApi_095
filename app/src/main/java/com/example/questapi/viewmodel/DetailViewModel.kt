ce6de6990928aa44f44b0a9da1c667bb546aa4d6
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 14:45:47 +0700

    membuat class DetailViewModel pada file DetailViewModel.kt

--- app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -1,9 +1,27 @@
 package com.example.questapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi.modeldata.DetailSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import com.example.questapi.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUiDetail{
     data class Succes(val satusiswa: DetailSiswa): StatusUiDetail
     object Error : StatusUiDetail
     object Loading : StatusUiDetail
+}
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+    init {
+        getSatuSiswa()
+    }
 }
