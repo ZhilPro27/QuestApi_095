d25ceac82e3e9664249a5be747d579bcbc9c31a0
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 14:26:44 +0700

    membuat file EditViewModel.kt

---
+++ app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.questapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi.modeldata.UIStateSiswa
+import com.example.questapi.modeldata.toUiStateSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import com.example.questapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+}
