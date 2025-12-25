13e572b93ceb9f6227f813ed9b19022a0adc8a10
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 14:35:12 +0700

    menambahkan fungsi editSatuSiswa pada file EditViewModel.kt

--- app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi.modeldata.DetailSiswa
 import com.example.questapi.modeldata.UIStateSiswa
+import com.example.questapi.modeldata.toDataSiswa
 import com.example.questapi.modeldata.toUiStateSiswa
 import com.example.questapi.repositori.RepositoryDataSiswa
 import com.example.questapi.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class EditViewModel(
     savedStateHandle: SavedStateHandle,
@@ -36,6 +38,18 @@ class EditViewModel(
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState){
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+           val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            } else {
+                println("Update Error : ${call.errorBody()}")
+            }
         }
     }
 }
