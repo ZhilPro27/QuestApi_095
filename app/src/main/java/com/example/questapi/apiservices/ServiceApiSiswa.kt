f6d5b6c07d5ca3ab74597bf18ded37a88cadfc5e
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 13:52:19 +0700

    Menambahkan 1 method PUT untuk endpoint editTM.php/:id pada file ServiceApiSiswa.kt

--- app/src/main/java/com/example/questapi/apiservices/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi/apiservices/ServiceApiSiswa.kt
@@ -4,6 +4,7 @@ import com.example.questapi.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {
@@ -16,4 +17,6 @@ interface ServiceApiSiswa {
     @GET("baca1Teman.php/{id}")
     suspend fun getStatusSiswa(@Query("id") id: Int): DataSiswa
 
+    @PUT("editTM.php/{id}")
+    suspend fun editStatusSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
