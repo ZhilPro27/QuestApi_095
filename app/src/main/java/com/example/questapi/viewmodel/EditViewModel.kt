4466681a79c6d10230a0162ab601a8ca28d320a6
ZhilPro27 <zhilalkrisna@gmail.com>
2025-12-25 14:28:55 +0700

    menambahkan fungsi updateUiState pada file EditViewModel.kt

--- app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.questapi.modeldata.DetailSiswa
 import com.example.questapi.modeldata.UIStateSiswa
 import com.example.questapi.modeldata.toUiStateSiswa
 import com.example.questapi.repositori.RepositoryDataSiswa
@@ -25,5 +26,10 @@ class EditViewModel(
             uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
                 .toUiStateSiswa(true)
         }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 }
